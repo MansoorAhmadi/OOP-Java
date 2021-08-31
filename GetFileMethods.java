351675936dfe34ac4ced19b39bed0841c0c76d4f
MansoorAhmadi <82879983+MansoorAhmadi@users.noreply.github.com>
2021-08-31 11:28:04 +0200

    Update GetFileMethods.java
    Added another API FileWriter and Write() Method

--- GetFileMethods.java
+++ GetFileMethods.java
@@ -2,12 +2,14 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class GetFileInfo {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         File myObj = new File("test.docx");
         if (myObj.exists()) {
             System.out.println("File name: " + myObj.getName());
@@ -15,18 +17,20 @@ public class GetFileInfo {
             System.out.println("Writeable: " + myObj.canWrite());
             System.out.println("Readable " + myObj.canRead());
             System.out.println("File size in bytes " + myObj.length());
+            System.out.println();
         } else {
             System.out.println("The file does not exist.");
             System.out.println();
         }
-        Scanner scanner = new Scanner(myObj);
-        List<String> myList = new ArrayList<>();
-        String line = "";
 
-        //scanner.nextLine();
+        FileWriter myWriter = new FileWriter("test.docx");
+        myWriter.write("I am adding another line to the test.docx file using FileWrite API and .write() method");
+        myWriter.close();
+
+        Scanner scanner = new Scanner(myObj);
         while (scanner.hasNext()) {
-            line = scanner.nextLine();
-            System.out.println(line);
+            String data = scanner.nextLine();
+            System.out.println(data);
         }
         scanner.close();
     }
@@ -40,4 +44,4 @@ OUTPUT:
 //canRead() will return a boolean as well
 //length() will return the length of the file
 
-//If the file is not imported into the project, for sure it will return the else statement.
+//I am adding another line to the test.docx file using FileWrite API and .write() method.
